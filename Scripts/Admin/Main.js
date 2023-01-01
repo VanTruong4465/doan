b1ed852271d0cd0ac9d2fac24bf09addd7499541
VanTruong4465 <114282235+VanTruong4465@users.noreply.github.com>
2023-01-02 00:04:28 +0700

    Add files via upload

---
+++ Scripts/Admin/Main.js
@@ -0,0 +1,7 @@
+﻿function ToogleList() {
+    document.getElementById("OpenList").classList.toggle("Hide");
+    document.getElementById("CloseList").classList.toggle("Hide");
+    document.getElementById("SideBar").classList.toggle("SideBarShow");
+    document.getElementById("ToogleList").classList.toggle("HeaderListBarShow");
+    var Status = document.getElementById("OpenList").classList.value;
+}
